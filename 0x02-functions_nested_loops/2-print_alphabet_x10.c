b3ae6ba90bab6aa92e5fc654917194600fdffc96
Oghenero Ologe <nologe37@gmail.com>
2022-06-07 04:57:27 -0700

    task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,7 +2,8 @@
 
 /**
  * print_alphabet_x10 - Check description
- * Description: It prints the alphabet in lowercase 10 times followed by a new line
+ * Description: It prints the alphabet in lowercase
+ * 10 times followed by a new line
  * Return. Nothing
  */
 void print_alphabet_x10(void)
