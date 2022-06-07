be1902dac6e305e1a20ceb8221532da45a2f7bb4
Oghenero Ologe <59165938+Nero2005@users.noreply.github.com>
2022-06-07 13:21:42 +0100

    Update 6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,6 +1,10 @@
 #include "holberton.h"
 
 /**
+ *_abs - Check Holberton
+ * @r: An integre input
+ * Description: This function returns absolute value of a number
+ * Return: Absolut value of number r
  */
 int _abs(int r)
 {
