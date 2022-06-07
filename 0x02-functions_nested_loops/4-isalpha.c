f91e712f2ee914b6fad11fb1dd53c71c486131e5
Oghenero Ologe <nologe37@gmail.com>
2022-06-07 05:10:28 -0700

    task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * _islower - Check Holberton
+ * @c: An input character
+ * Description: function uses _putchar function to print
+ * alphabet in lowercase 10 times
+ * Return: 1 if is lowercase or 0 if is uppercase
+ */
+int _isalpha(int c)
+{
+	char lower, upper;
+	int isletter = 0;
+
+	for (lower = 'a'; lower <= 'z'; lower++)
+	{
+		for (upper = 'A'; upper <= 'Z'; upper++)
+		{
+			if (lower == c || upper == c)
+				isletter = 1;
+	}
+
+	return (isletter);
+}
