faa9f510ed0d346f9104690594369b84bc261835
Oghenero Ologe <59165938+Nero2005@users.noreply.github.com>
2022-06-07 08:19:06 +0100

    Create 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Check description
+ * Description: It prints the word _putchar, followed by a new line.
+ * Return: 0.
+ */
+int main(void)
+{
+	printf("_putchar");
+
+	return (0);
+}
