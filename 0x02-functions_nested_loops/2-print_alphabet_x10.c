72dd7990d0e1a3d80aea134562be30994a2e881e
Oghenero Ologe <nologe37@gmail.com>
2022-06-07 04:48:54 -0700

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * print_alphabet_x10 - Check description
+ * Description: It prints the alphabet in lowercase 10 times followed by a new line
+ * Return. Nothing
+ */
+void print_alphabe_x10t(void)
+{
+	char i;
+	int j;
+
+	for (j = 0; j < 10; j++)
+	{
+		for (i = 'a'; i <= 'z'; i++)
+			_putchar(i);
+		_putchar('\n');
+	}
+}
