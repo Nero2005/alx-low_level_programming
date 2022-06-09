832d419dc1ccefe13474cd7c7156b8cc47d4ebec
Oghenero Ologe <59165938+Nero2005@users.noreply.github.com>
2022-06-09 12:13:44 +0100

    Update 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,17 +5,18 @@
    * followed by a new line.
    * Return: Always 0
  */
-
 void more_numbers(void)
 {
-	int i, j;
-	for (i = 0; i <= 9; i++)
+	int i, j = 0;
+
+	for (; j < 10; j++)
 	{
-		for (j = 0; j <= 14; j++)
+		for (i = 0; i <= 14; i++)
 		{
-			if (j > 9)
-				_putchar(j / 10 + '0');
-			_putchar(j % 10 + '0');
+			if (i > 9)
+				_putchar(i / 10 + '0');
+
+			_putchar(i % 10 + '0');
 		}
 		_putchar('\n');
 	}
