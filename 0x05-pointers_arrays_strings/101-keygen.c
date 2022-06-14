7a8a36be62ad4125590d0ee66c07ab23889ec80b
Oghenero Ologe <59165938+Nero2005@users.noreply.github.com>
2022-06-14 18:36:22 +0100

    Update 101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,12 +1,36 @@
-#include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 /**
+ * main - generates random valid passwords
+ *
+ * Return: Always 0
  */
 
-char[] main(void)
+int main(void)
 {
-	char[8] psw = "Nero@mdx";
+	char k[200];
+	int num = 0;
+	int random = 0;
+	char *key = k;
 
-	return (psw);
+	srand(time(NULL));
+
+	while (num < 2645)
+	{
+		random = rand() % 122;
+
+		if (random > 32)
+		{
+			*key = random;
+			key = key + 1;
+			num += random;
+		}
+	}
+
+	*key = (2772 - num);
+	*(key + 1) = '\n';
+	printf("%s", k);
+	return (0);
 }
