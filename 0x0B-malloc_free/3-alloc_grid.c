fd6cd00106069eab45f264c49ed0383a2a4433ad
Oghenero Ologe <59165938+Nero2005@users.noreply.github.com>
2022-06-27 13:10:19 +0100

    Update 3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,39 +1,40 @@
-#include "main.h"
 #include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
 
 /**
- * alloc_grid - A function that returns a pointer to a
- * 2 dimensional array of integers
- * @width: An input integer at number of columns
- * @height: An input integer at number of rows
- * Return: pointer to a 2D array, NULL on failure
+ * alloc_grid - returns a pointer to a 2 dimensional array of integers
+ * @width: columns
+ * @height: rows
+ * Return: pointer to 2d array
  */
+
 int **alloc_grid(int width, int height)
 {
-	int **array, i = 0, j = 0;
+	int **grid;
+	int i, j;
 
-	if (width <= 0 || height <= 0)
-	{
+	if (width <= 0 || height <= 0) /* validate input */
 		return (NULL);
-	}
-	array = (int **)malloc(sizeof(int *) * height);
-	if (array == NULL)
-	{
-		free(array);
+
+	grid = malloc(height * sizeof(int *)); /*allocate memory for rows*/
+
+	if (grid == NULL) /* validate memory */
 		return (NULL);
-	}
-	for (; i < height; i++)
+
+	for (i = 0; i < height; i++) /*allocate memory for columns of each row*/
 	{
-		array[i] = (int *)malloc(sizeof(int) * width);
-		if (array[i] == NULL)
+		grid[i] = malloc(width * sizeof(int));
+		if (grid[i] == NULL) /* validate memory */
 		{
-			free(array);
+			for (i = 0; i < height; i++)
+				free(grid[i]);
+			free(grid);
 			return (NULL);
 		}
+		for (j = 0; j < width; j++) /* set array values to 0 */
+			grid[i][j] = 0;
 	}
 
-	for (i = 0; i < height; i++)
-		for (; j < width; j++)
-			array[i][j] = 0;
-	return (array);
+	return (grid);
 }
