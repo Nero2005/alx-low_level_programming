f806e6f561ab1e69b6ff36a44eb9856b558cdff6
Oghenero Ologe <59165938+Nero2005@users.noreply.github.com>
2022-06-27 12:59:37 +0100

    task 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -13,16 +13,23 @@ int **alloc_grid(int width, int height)
 	int **array, i = 0, j = 0;
 
 	if (width <= 0 || height <= 0)
+	{
 		return (NULL);
-
+	}
 	array = (int **)malloc(sizeof(int *) * height);
 	if (array == NULL)
+	{
+		free(array);
 		return (NULL);
+	}
 	for (; i < height; i++)
 	{
 		array[i] = (int *)malloc(sizeof(int) * width);
 		if (array[i] == NULL)
+		{
+			free(array[i]);
 			return (NULL);
+		}
 	}
 
 	for (i = 0; i < height; i++)
