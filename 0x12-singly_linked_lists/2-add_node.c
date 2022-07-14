8e9a6fefc3c3f7cfc974b9cb97cb6f6e0f81b70f
Oghenero Ologe <59165938+Nero2005@users.noreply.github.com>
2022-07-14 13:26:20 +0100

    Update 2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,31 +1,27 @@
-#include <stdlib.h>
-#include <string.h>
-#include "strlen.c"
 #include "lists.h"
+#include <string.h>
 
 /**
- * add_node - Adds a new node at the beginning of list_t list
- * @new_head: Address of node
- * @str: New node to add
- * Return: Address of new element, NULL if it fails
+ * add_node - A function that adds a node to the head of linked list
+ * @head: pointer to a list_t pointer that points to the head struct
+ * @str: pointer to string that adds as node
+ * Return: pointer to new head of list, NULL on failure
  */
-
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new_node_ptr = malloc(sizeof(list_t));
+	list_t *temp;
+	int length = 0;
 
-	new_head = malloc(sizeof(list_t));
-
-	if (new_head == NULL)
-	{
+	temp = malloc(sizeof(list_t));
+	if (temp == NULL)
 		return (NULL);
-	}
-	else
-	{
-		new_head->str = strdup(str);
-		new_head->len = length;
-		new_head->next = *head;
-		*head = new;
-		return (new_head);
-	}
+
+	while (str[length])
+		length++;
+
+	temp->len = length;
+	temp->str = strdup(str);
+	temp->next = *head;
+	*head = temp;
+	return (temp);
 }
